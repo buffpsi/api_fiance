f1173388c09e22df226e26513ac0f05b9aaa4c7b
본ìbuffpsigit config --global user.email 깃허브 <rlawlstjd778@naver.com>
2023-05-30 07:16:28 +0900

    alphaadvantageã 추가

--- src/main/java/com/ll/yahooFiance/AlphaAdvantage/AlphaAdvantageController.java
+++ src/main/java/com/ll/yahooFiance/AlphaAdvantage/AlphaAdvantageController.java
@@ -1,4 +1,26 @@
 package com.ll.yahooFiance.AlphaAdvantage;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/stock")
 public class AlphaAdvantageController {
+
+    private final AlphaAdvantageService alphaAdvantageService;
+
+    @Autowired
+    public AlphaAdvantageController(AlphaAdvantageService alphaAdvantageService) {
+        this.alphaAdvantageService = alphaAdvantageService;
+    }
+
+    @GetMapping("/{symbol}/historical-prices")
+    public List<HistoricalPrice> getHistoricalPrices(@PathVariable String symbol) {
+        return alphaAdvantageService.getHistoricalPrices(symbol);
+    }
 }
